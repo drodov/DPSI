f67089eda34fb44f5262a51e0c518f82e6235074
Dmitry <DmitryRodov@ya.ru>
2013-02-03 02:40:41 +0300

    Lab 8-1: Grey and binary image

---
+++ Sem8/DPSI_lab_1/DPSI_lab_1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using ImageHelper;
+using Microsoft.Win32;
+
+namespace DPSI_lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage _sourceImage;
+        private BitmapImage _greyImage;
+        private BitmapImage _binaryImage;
+        private BitmapImage _recognizedImage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+                {
+                    InitialDirectory = @"C:\Users\Dmitry\Pictures",
+                    FilterIndex = 2,
+                    RestoreDirectory = true
+                };
+
+            if (openFileDialog.ShowDialog() != null)
+            {
+                try
+                {
+                    _sourceImage = new BitmapImage(new Uri(openFileDialog.FileName));
+                    SourceImage.Source = _sourceImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+                try
+                {
+                    _greyImage = ImageConverter.GetGreyImage(_sourceImage);
+                    GreyImage.Source = _greyImage;
+
+                    _binaryImage = ImageConverter.GetBinaryImage(_sourceImage);
+                    BinaryImage.Source = _binaryImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error in image processing: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Sem8/DPSI_lab_1/ImageHelper/ImageConverter.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace ImageHelper
+{
+    public static class ImageConverter
+    {
+        public static Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                var enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return new Bitmap(bitmap);
+        }
+
+        public static BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage;
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
+        }
+
+        public static BitmapImage GetGreyImage(BitmapImage sourceBitmap)
+        {
+            int height = sourceBitmap.PixelHeight;
+            int width = sourceBitmap.PixelWidth;
+
+            Bitmap bitmap = BitmapImage2Bitmap(sourceBitmap);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var hsv = new HSV(bitmap.GetPixel(j, i));
+                    bitmap.SetPixel(j, i, (new HSV(0, 0, hsv.Value)).ToColor());
+                }
+            }
+            return Bitmap2BitmapImage(bitmap);
+        }
+
+        public static BitmapImage GetBinaryImage(BitmapImage sourceBitmap)
+        {
+            int height = sourceBitmap.PixelHeight;
+            int width = sourceBitmap.PixelWidth;
+
+            var brightnessesNumber = new int[256];
+            int pixelsNumber = height*width;
+
+            Bitmap bitmap = BitmapImage2Bitmap(sourceBitmap);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var hsv = new HSV(bitmap.GetPixel(j, i));
+                    brightnessesNumber[hsv.Value]++;
+                }
+            }
+
+            int treshhold = brightnessesNumber.Select((t, i) => i*t).Sum() / pixelsNumber;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var hsv = new HSV(bitmap.GetPixel(j, i));
+                    bitmap.SetPixel(j, i, (new HSV(0, 0, hsv.Value >= treshhold ? (byte)255 : (byte)0)).ToColor());
+                }
+            }
+
+            return Bitmap2BitmapImage(bitmap);
+        }
+    }
+}
